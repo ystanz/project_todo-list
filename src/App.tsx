4fc741df3cc1a8817f9674bd08bb455a390ce351
Yuri Stanzani <ystanzani@gmail.com>
2025-03-18 14:44:04 -0300

    feat: install React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import { Provider } from 'react-redux'
-import SideBar from './containers/SideBar'
-import TasksList from './containers/TasksList'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import GlobalStyle, { Container } from './styles'
 import store from './store'
+import Home from './pages/Home'
+import Register from './pages/Register'
 
+const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/novo',
+    element: <Register />
+  }
+])
 function App() {
   return (
     <Provider store={store}>
       <GlobalStyle />
       <Container>
-        <SideBar />
-        <TasksList />
+        <RouterProvider router={routes} />
       </Container>
     </Provider>
   )
